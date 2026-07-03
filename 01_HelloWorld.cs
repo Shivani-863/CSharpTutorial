6f804fd1ee4f79cca4e16c1aff0cecbbe81c5fd6
Shivani Kamble <shivanikamble083@gmail.com>
2026-07-03 08:38:19 +0530

    First Program

---
+++ 01_HelloWorld.cs
@@ -0,0 +1,15 @@
+using System;
+// using imports a namespace so you can use its classes without writing their full names.
+// A namespace groups related classes together.
+public class HelloWorld
+{
+public void Hello()
+    {
+       Console.WriteLine("Hello World!");
+       Console.WriteLine("Hey, I'm Shivani Here!");
+    // The Console class is inside the System namespace.
+    // A built-in class used for interacting with the console.
+    // WriteLine
+    // A method that prints text to the console and then moves the cursor to the next line.
+    }
+}
